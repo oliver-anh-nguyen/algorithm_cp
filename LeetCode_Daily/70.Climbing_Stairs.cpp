3adb59f341c393a5c3a8bb8af4cf44e4dea2d3c9
tuan.anh.nguyen <oliver.anh.nguyen@gmail.com>
2024-01-17 21:40:04 -0600

    Leetcode: 70

---
+++ LeetCode_Daily/70.Climbing_Stairs.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        vector<int> dp(n + 1);
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+};
